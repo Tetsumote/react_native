969f4d9106c5a56a6006fe92f95818704ad9ac03
Paweł Grajewski <unionproject88@gmail.com>
2019-10-17 17:47:49 +0200

    add state

--- App.js
+++ App.js
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! and have fun.</Text>
-    </View>
-  );
+
+class Greeting extends Component{
+  render(){
+    return(
+      <View style={styles.greeting}>
+        <Text>Hello {this.props.name}!</Text>
+      </View>
+    )
+  }
 }
+class Blink extends Component{
+  componentDidMount(){
+    setInterval(() => (
+      this.setState(previousState => (
+        {isShowingText: !previousState.isShowingText}
+      ))
+    ),3000)
+  }
+  state = {isShowingText:true}
+  render(){
+    if(!this.state.isShowingText){
+      return null;
+    }
+    return (
+      <Text>{this.props.text}</Text>
+    );
+  } 
+}
+export default class Bananas extends Component {
+  render(){
+
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    }
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app! and have fun.</Text>
+        <Image source={pic} style={{ width:193, height:110 }} />
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+        <Blink text='I love to blink'/>
+      </View>
+    );
+  }
+} 
 
 const styles = StyleSheet.create({
   container: {
@@ -16,4 +56,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  greeting:{
+    alignItems:'center'
+  }
 });
