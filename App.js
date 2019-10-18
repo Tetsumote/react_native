5720c838e4632025dc7c55e215b179689ed1fe4d
Paweł Grajewski <unionproject88@gmail.com>
2019-10-18 23:02:40 +0200

    add textInput

--- App.js
+++ App.js
@@ -1,62 +1,33 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Image } from 'react-native';
+import { Text,  TextInput, View } from 'react-native';
 
 
-class Greeting extends Component{
-  render(){
-    return(
-      <View style={styles.greeting}>
-        <Text>Hello {this.props.name}!</Text>
-      </View>
-    )
+
+
+export default class PizzaTranslator extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {text: ''}
   }
-}
-class Blink extends Component{
-  componentDidMount(){
-    setInterval(() => (
-      this.setState(previousState => (
-        {isShowingText: !previousState.isShowingText}
-      ))
-    ),3000)
-  }
-  state = {isShowingText:true}
-  render(){
-    if(!this.state.isShowingText){
-      return null;
-    }
-    return (
-      <Text>{this.props.text}</Text>
-    );
-  } 
-}
-export default class Bananas extends Component {
+
   render(){
 
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    }
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app! and have fun.</Text>
-        <Image source={pic} style={{ width:193, height:110 }} />
-        <Greeting name='Rexxar' />
-        <Greeting name='Jaina' />
-        <Greeting name='Valeera' />
-        <Blink text='I love to blink'/>
+      <View style={{ padding:20 }}>
+        <TextInput
+          style={{ height:40 }}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+        />
+        <Text style={{padding:10, fontSize:42 }}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
+
       </View>
     );
   }
 } 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  greeting:{
-    alignItems:'center'
-  }
-});
+
