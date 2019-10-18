6a1606b51b793be24ccef6b55e9f30174b7f3ec8
Paweł Grajewski <unionproject88@gmail.com>
2019-10-18 18:46:16 +0200

    add flex branch

--- App.js
+++ App.js
@@ -1,62 +1,21 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Image } from 'react-native';
+import { View } from 'react-native';
 
 
-class Greeting extends Component{
+
+
+export default class FlexDimensionsBasics extends Component {
   render(){
-    return(
-      <View style={styles.greeting}>
-        <Text>Hello {this.props.name}!</Text>
-      </View>
-    )
-  }
-}
-class Blink extends Component{
-  componentDidMount(){
-    setInterval(() => (
-      this.setState(previousState => (
-        {isShowingText: !previousState.isShowingText}
-      ))
-    ),3000)
-  }
-  state = {isShowingText:true}
-  render(){
-    if(!this.state.isShowingText){
-      return null;
-    }
+
+
     return (
-      <Text>{this.props.text}</Text>
-    );
-  } 
-}
-export default class Bananas extends Component {
-  render(){
-
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    }
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app! and have fun.</Text>
-        <Image source={pic} style={{ width:193, height:110 }} />
-        <Greeting name='Rexxar' />
-        <Greeting name='Jaina' />
-        <Greeting name='Valeera' />
-        <Blink text='I love to blink'/>
+      <View style={{ flex:1 }}>
+        <View style={{ flex: 1, backgroundColor:'powderblue' }} />
+        <View style={{ flex: 2, backgroundColor:'skyblue' }} />
+        <View style={{ flex: 3, backgroundColor:'steelblue' }} />
       </View>
     );
   }
 } 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  greeting:{
-    alignItems:'center'
-  }
-});
+
