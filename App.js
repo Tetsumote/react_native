791ab0c9049ae181a0e1e5a94fb5fdb3c0cfd695
Paweł Grajewski <unionproject88@gmail.com>
2019-10-18 18:31:02 +0200

    add styles component

--- App.js
+++ App.js
@@ -1,62 +1,30 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Image } from 'react-native';
 
-
-class Greeting extends Component{
-  render(){
-    return(
-      <View style={styles.greeting}>
-        <Text>Hello {this.props.name}!</Text>
-      </View>
-    )
+const styles = StyleSheet.create({
+  bigBlue: {
+    color:'blue',
+    fontWeight:'bold',
+    fontSize:30,
+  },
+  red:{
+    color:'red',
+    marginTop:50,
   }
-}
-class Blink extends Component{
-  componentDidMount(){
-    setInterval(() => (
-      this.setState(previousState => (
-        {isShowingText: !previousState.isShowingText}
-      ))
-    ),3000)
-  }
-  state = {isShowingText:true}
-  render(){
-    if(!this.state.isShowingText){
-      return null;
-    }
-    return (
-      <Text>{this.props.text}</Text>
-    );
-  } 
-}
-export default class Bananas extends Component {
+});
+
+export default class LotsOfStyles extends Component {
   render(){
 
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    }
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app! and have fun.</Text>
-        <Image source={pic} style={{ width:193, height:110 }} />
-        <Greeting name='Rexxar' />
-        <Greeting name='Jaina' />
-        <Greeting name='Valeera' />
-        <Blink text='I love to blink'/>
+      <View>
+        <Text style={styles.red}>just red</Text>
+        <Text style={styles.bigBlue}>just bigBlue</Text>
+        <Text style={[styles.bigBlue,styles.red]}>bigBlue, then red</Text>
+        <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
       </View>
     );
   }
 } 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  greeting:{
-    alignItems:'center'
-  }
-});
+
