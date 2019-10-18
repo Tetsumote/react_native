d6549838d120e56542d3cf8a3f7e4abd9649c203
Paweł Grajewski <unionproject88@gmail.com>
2019-10-18 21:39:40 +0200

    add justifyContent

--- App.js
+++ App.js
@@ -4,13 +4,19 @@ import { View } from 'react-native';
 
 
 
-export default class FlexDimensionsBasics extends Component {
+export default class JustifyContentBasics extends Component {
   render(){
 
 
     return (
-
-      <View style={{ flex:1,flexDirection:'row' }}>   
+      // Try setting `justifyContent` to `center`.
+      // Try setting `flexDirection` to `row`.
+      // Options: flex-start , center, flex-end, space-between, space-around
+      <View style={{ 
+          flex:1,
+          flexDirection:'column',
+          justifyContent: 'space-around',
+         }}>   
         <View style={{ width:50, height:50, backgroundColor:'powderblue' }} />
         <View style={{ width:50, height:50, backgroundColor:'skyblue' }} />
         <View style={{ width:50, height:50, backgroundColor:'steelblue' }} />
