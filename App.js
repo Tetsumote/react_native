f3671af84f5fef28cd6f9c95793bc2a6e4da24ee
Paweł Grajewski <unionproject88@gmail.com>
2019-10-22 17:43:52 +0200

    add scroll view

--- App.js
+++ App.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
-import { Platform, Text, TouchableHighlight, 
-  TouchableOpacity,TouchableNativeFeedback, 
-  TouchableWithoutFeedback, 
-  Button, StyleSheet, View } from 'react-native';
+import { ScrollView, Image,  Text } from 'react-native';
 
 
-
-
-export default class Touchables extends Component {
+export default class IScrolledDownAndWhatHappenedNextShockedMe extends Component {
   _onPressButton(){
     alert('You tapped the button!')
   }
@@ -17,50 +12,41 @@ export default class Touchables extends Component {
   render(){
 
     return (
-      <View style={styles.container}>
-        <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableHighlight</Text>
-          </View>
-        </TouchableHighlight>
-        <TouchableOpacity onPress={this._onPressButton}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableOpacity</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableNativeFeedback
-        onPress={this._onPressButton}
-        background={Platform.OS === 'android' ?
-        TouchableNativeFeedback.SelectableBackground() : ''
-        }>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableNativeFeedback {Platform.OS !== 'android' ? '(Android only)' : ''}</Text>
-          </View>
-        </TouchableNativeFeedback>
-        <TouchableHighlight onPress={this._onPressButton} onLongPress={this._onLongPressButton} underlayColor="white">
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>Touchable with Long Press</Text>
-          </View>
-        </TouchableHighlight>
-      </View>
+      <ScrollView>
+        <Text style={{fontSize:96}}>Scroll me plz</Text>
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Text style={{fontSize:96}}>If you like</Text>
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Text style={{fontSize:96}}>Scrolling dowm</Text>
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Text style={{fontSize:96}}>What's the best</Text>
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Text style={{fontSize:96}}>Framework around?</Text>
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Image source={{ uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width:64,height:64 }} />
+        <Text style={{fontSize:96}}>React Native</Text>
+      </ScrollView>
     );
   }
 } 
 
-const styles = StyleSheet.create({
-  container: {
-    paddingTop:60,
-    alignItems: 'center',
-  },
-  button:{
-    marginBottom:30,
-    width:260,
-    alignItems:'center',
-    backgroundColor:'#2196F3'
-  },
-  buttonText:{
-    textAlign:'center',
-    padding:20,
-    color:'white'
-  }
-});
+
